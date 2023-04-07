dc17e90d0aeeb817e9a4f7bd41639bc6942f5d8b
ztzzhi <9972792+zhangtingzhi@user.noreply.gitee.com>
2023-04-07 15:12:11 +0800

    update:修复dev文件丢失的问题

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 export const headerImg = "header.jpg"
 //图片上传地址
-export const imgUploadUrl = "http://www.ditnow.cn:7001/upload"
+export const imgUploadUrl = "http://www.ditnow.cn:7001/upload" //换成本地（http://localhost:7001/upload）或者线上你的ip:7001/upload的地址 这是我的服务器地址 大家最好不要上传到我的服务器～
 // 文章上传密钥
-export const secret = "ztzweb"
+export const secret = "ztzweb" // 文章发布所需要的密钥
